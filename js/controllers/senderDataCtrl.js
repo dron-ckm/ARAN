9a60eaf7ea895da0871141969ea340bb73cb505b
dmz9 <dmz9@yandex.ru>
2016-12-30 17:56:01 +0300

    senderData rework - small fixes

--- js/controllers/senderDataCtrl.js
+++ js/controllers/senderDataCtrl.js
@@ -2,27 +2,26 @@ app.controller('senderDataCtrl', ['$scope', 'SenderData', function($scope, Sende
     $scope.unSaved=false;
 	function selfUpdate(response) {
         $scope.senders = SenderData.parse(response);
+        $scope.unSaved=false;
     }
 	SenderData.getData().then(selfUpdate);
 	$scope.isChangeEvailable = false;
 	$scope.toggleEdit = function(){
 		$scope.isChangeEvailable = !$scope.isChangeEvailable;
 	};
-	$scope.rmSender = function (id,$event) {
+	$scope.rmSender = function (sender,$event) {
         $event.stopPropagation();
         $event.preventDefault();
-        if (id) {
-            $scope.senders.splice(id,1);
-        }
+		_.remove($scope.senders, sender);
 		return false;
     };
 	$scope.saveAll=function () {
 		SenderData.saveAll($scope.senders).then(selfUpdate);
-		$scope.unSaved=false;
     };
     $scope.$watch("senders", function(newVal,oldVal){
     	// при инициализации не сработает лишний раз
 		$scope.unSaved = (newVal&&oldVal&&newVal!==oldVal);
+		console.log('sender watch triggered');
     }, true);
 	$scope.addSender = function(){
         $scope.senders.push({
