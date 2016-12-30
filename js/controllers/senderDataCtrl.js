1c582a8ca72fc583220fc5651fe8f983b3651047
dmz9 <dmz9@yandex.ru>
2016-12-30 17:41:04 +0300

    senderData rework

--- js/controllers/senderDataCtrl.js
+++ js/controllers/senderDataCtrl.js
@@ -1,4 +1,5 @@
 app.controller('senderDataCtrl', ['$scope', 'SenderData', function($scope, SenderData){
+    $scope.unSaved=false;
 	function selfUpdate(response) {
         $scope.senders = SenderData.parse(response);
     }
@@ -10,19 +11,23 @@ app.controller('senderDataCtrl', ['$scope', 'SenderData', function($scope, Sende
 	$scope.rmSender = function (id,$event) {
         $event.stopPropagation();
         $event.preventDefault();
-		SenderData.rmSender(id).then(selfUpdate);
+        if (id) {
+            $scope.senders.splice(id,1);
+        }
 		return false;
     };
 	$scope.saveAll=function () {
 		SenderData.saveAll($scope.senders).then(selfUpdate);
+		$scope.unSaved=false;
     };
+    $scope.$watch("senders", function(newVal,oldVal){
+    	// при инициализации не сработает лишний раз
+		$scope.unSaved = (newVal&&oldVal&&newVal!==oldVal);
+    }, true);
 	$scope.addSender = function(){
-		SenderData.addData({
-			city: '3M',
-			addressFrom: 'asd',
-			phone: '800',
-			contactName: 'name',
-			shopName: 'shop'
-		}).then(selfUpdate);
+        $scope.senders.push({
+			id:$scope.senders.length,
+			new:true
+		});
 	}
 }]);
--- js/services/SenderData.js
+++ js/services/SenderData.js
@@ -1,54 +1,68 @@
 app.service('SenderData', ['$http','Restangular','$cacheFactory', function($http,Restangular,$cacheFactory){
-    var companyCache={};
+    var nameAndPhoneDelimiter=" | ";
+    var companyCache={};// restangular объект, наполняется методами самим restangular-ом и содержит response-данные
 	var _rest = Restangular.withConfig(config).service("companies");
+    var restCompanyCache = $cacheFactory("companyCache"); // rest cache
     function config(RestangularConfigurer) {
         RestangularConfigurer.setDefaultHttpFields({
-            cache: companyCached
+            cache: restCompanyCache
         });
-        RestangularConfigurer.setResponseExtractor(function (t, e, o) {
-            return "nomenclature" === o ? t.nomenclatures : "getList" === e ? t.companies : [
+        RestangularConfigurer.setResponseExtractor(function (response, operation) {
+            if ("getList" === operation) {
+                return response.companies
+            } else {
+                if ([
                         "get",
                         "post",
                         "put",
                         "save"
-                    ].indexOf(e) >= 0 ? t.company : t
+                    ].indexOf(operation) >= 0) {
+                    return response.company
+                } else {
+                    return response
+                }
+            }
+
         });
         RestangularConfigurer.setErrorInterceptor(function (response, e) {
-            if (response.data && response.data.msg) {
-                response.data.msg
-            }
             e.reject()
         });
         RestangularConfigurer.addElementTransformer("companies", true, function (element) {
-            return element.addRestangularMethod("getCompany", "get", "me"), element
+            element.addRestangularMethod("getCompany", "get", "me");
+            return element
         });
-        RestangularConfigurer.addResponseInterceptor(function (data, operation, what, url, response, n) {
-            if (response.data && response.data.msg) {
-                response.data.msg
-            }
+        RestangularConfigurer.addResponseInterceptor(function (data, operation) {
             if ([
                     "put",
                     "post",
                     "delete"
                 ].indexOf(operation) >= 0) {
-                companyCached.removeAll()
+                restCompanyCache.removeAll()
             }
             return data
         })
     }
-    var companyCached = $cacheFactory("companyCache"); // rest cache
     function transformSender(sender) {
+        // из контроллера в формат апи
         return {
-            apartment:null,
-            building:null,
-            city:sender.city,
-            district:null,
-            house:null,
-            housing:null,
-            name:sender.shopName,
-            region:null,
-            representative:sender.contactName,
-            street:sender.addressFrom
+            apartment:sender.apartment, // квартира/офис
+            building:sender.building,// строение
+            city:sender.city,// город
+            district:sender.district,// район, хз район города либо район как часть области
+            house:sender.house, // дом
+            housing:sender.housing,// корпус
+            name:sender.shopName,// название организации/магазина/ооо
+            postal_code:sender.postal_code,//индекс
+            region:sender.region,// регион/область
+            // Представитель (ФИО), но будем пока запихивать туда и телефон через разделитель
+            representative:(function (name,phone) {
+                var strArr=[];
+                (name&&name.length)?strArr.push(name):true;
+                (phone&&phone.length)?strArr.push(phone):true;
+                return strArr.join(nameAndPhoneDelimiter);
+            })(sender.contactName,sender.phone),
+            street:sender.street, // улица,
+            phone:sender.phone  // пока не работает
         };
     }
 	function addData(sender){
@@ -57,10 +71,8 @@ app.service('SenderData', ['$http','Restangular','$cacheFactory', function($http
     }
     function rmSender(id) {
         companyCache.places_loading.splice(id,1);
-        console.log(companyCache.places_loading.length);
         return companyCache.save();
     }
-
     function saveAll(senders) {
         if (senders.length) {
             companyCache.places_loading = senders.map(function (oneSender) {
@@ -70,24 +82,37 @@ app.service('SenderData', ['$http','Restangular','$cacheFactory', function($http
         return companyCache.save();
     }
 	function parse(response) {
+	    // разбор places_loading
 	    companyCache=response;
         return response.places_loading.map(function (place,id) {
+            // костыль - пока нет поля для телефона придется хранить в поле представителя
+            var nameAndPhone;
+            nameAndPhone=(place.representative&&place.representative.length)?place.representative.split(nameAndPhoneDelimiter):[];
             return {
                 id:id,
                 city: place.city,
-                addressFrom: [place.street,place.district,place.house].join(', '),
-                phone: ' ',
-                contactName: place.representative,
+                addressFrom: (function (street,house,office) {
+                    var str=(street&&street.length)?'ул. '+street:'';
+                    str+=(house&&house.length)?', д. '+house:'';
+                    str+=(office&&office.length)?', оф. '+office:'';
+                    return str;
+                })(place.street,place.house,place.apartment),
+                street:place.street,
+                house:place.house,
+                apartment:place.apartment,
+                phone: nameAndPhone[1]||' ',
+                contactName: nameAndPhone[0]||' ',
                 shopName: place.name
             }
         });
     }
+    function getData() {
+        return _rest.one("me").get();
+    }
 	return {
 	    parse:parse,
 		rmSender:rmSender,
-		getData:function () {
-            return _rest.one("me").get()
-        },
+		getData:getData,
 		saveAll:saveAll,
 		addData: addData
 	};
