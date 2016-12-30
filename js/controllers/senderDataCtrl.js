15af66638fb2cfbe5c2f432a8e27762de6b6145a
dmz9 <dmz9@yandex.ru>
2016-12-30 18:33:23 +0300

    event preventDefault for buttons[type=submit]

--- js/controllers/senderDataCtrl.js
+++ js/controllers/senderDataCtrl.js
@@ -8,8 +8,9 @@ app.controller('senderDataCtrl', ['$scope', 'SenderData', '$rootScope', function
     }
 	SenderData.getData().then(selfUpdate);
 	$scope.isChangeEvailable = false;
-	$scope.toggleEdit = function(){
-		$scope.isChangeEvailable = !$scope.isChangeEvailable;
+	$scope.toggleEdit = function($event){
+        $event.preventDefault();
+        $scope.isChangeEvailable = !$scope.isChangeEvailable;
 	};
 	$scope.rmSender = function (sender,$event) {
         $event.stopPropagation();
@@ -17,18 +18,19 @@ app.controller('senderDataCtrl', ['$scope', 'SenderData', '$rootScope', function
 		_.remove($scope.senders, sender);
 		return false;
     };
-	$scope.saveAll=function () {
-		SenderData.saveAll($scope.senders).then(selfUpdate);
+	$scope.saveAll=function ($event) {
+        $event.preventDefault();
+        SenderData.saveAll($scope.senders).then(selfUpdate);
     };
     $scope.$watch("senders", function(newVal,oldVal){
     	// при инициализации не сработает лишний раз
 		$scope.unSaved = (newVal&&oldVal&&newVal!==oldVal);
-		console.log('sender watch triggered');
     }, true);
-	$scope.addSender = function(){
-    $scope.senders.push({
-			id:$scope.senders.length,
-			new:true
-		});
-	}
+    $scope.addSender = function ($event) {
+        $event.preventDefault();
+        $scope.senders.push({
+            id: $scope.senders.length,
+            new: true
+        });
+    }
 }]);
